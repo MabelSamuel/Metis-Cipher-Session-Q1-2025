b10678adc8dcca45db3fbf0d242761eef142edb5
Mabel Samuel <mapleberrysamuel@gmail.com>
2025-02-25 23:21:09 +0100

    configured react routing

--- week-6-projects/mabel-samuel/frontend-voting-app/src/App.tsx
+++ week-6-projects/mabel-samuel/frontend-voting-app/src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import RootLayout from "./layouts/RootLayout";
+import HomePage from "./pages/HomePage";
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout/>}>
+          <Route index element={<HomePage/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
